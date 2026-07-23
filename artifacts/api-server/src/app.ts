a09411fd8e7c5d27b040460fa4f163fc928edeef
archanapri <archana1996.vtalent@gmail.com>
2026-07-23 12:52:46 +0530

    Clean up duplicate path import in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -3,8 +3,6 @@ import { fileURLToPath } from "url";
 import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
 import router from "./routes";
-import path from 'path';
-
 
 //const __filename = fileURLToPath(import.meta.url);
 //const __dirname  = path.dirname(__filename);
